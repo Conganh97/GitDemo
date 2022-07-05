7b407ee891777e9b614faaf835d053a20dd2e1f1
Conganh97 <anhbc.afsystem@gmail.com>
2022-07-05 10:06:06 +0700

    up

--- src/CongAnh.java
+++ src/CongAnh.java
@@ -1,2 +1,10 @@
 public class CongAnh {
+    private String name;
+    private int age;
+    private float height;
+    private float weight;
+
+    public static void main(String[] args) {
+        System.out.println("This is branch Cong Anh");
+    }
 }
